926fb4895c783a950780ff76d24c57cf548ec74e
PixelPusher1992 <yuriew.viktor@gmail.com>
2017-01-29 20:15:58 +0200

    poject started

---
+++ markup/header.php
@@ -0,0 +1,23 @@
+<?php
+require 'lib.php';
+require 'constants.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Page title</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
+    <meta HTTP-EQUIV="Refresh" сontent="3" URL="pages/main.php">
+    <link href="<?=SITE_DIR?>favicon.ico" rel="shortcut icon" type="image/x-icon" />
+    <link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH.'dist/css/style.min.css'?>"/>
+</head>
+<body>
+<?php include 'old-browser-warning.php'; ?>
+
+
+
+    
+    
+    
